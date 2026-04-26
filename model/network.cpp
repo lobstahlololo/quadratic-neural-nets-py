b400ab75875238526848ac2d1f37efae891fd0fc
supastishn <omareeto2012@hotmail.com>
2026-04-26 18:09:42 +0300

    begin exciting part: axtualcimplementsriom

---
+++ model/network.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <network.h>
+
+std::vector<float> weights;
+
+void Layer::forward(float* inputs) {
+	#ifdef TRAINING_ON
+		
+	#endif
+
+}
+
+#ifdef TRAINING_ON
+void Layer::backward(float* upstream_grad) {
+
+}
+#endif
+
+void main() {
+		
+}
--- model/network.h
+++ model/network.h
@@ -4,11 +4,15 @@
 
 #define TRAINING_ON
 
+typedef float (*HookFunc)(float);
 struct Layer {
 	float* weightsBegin;
 	float* current;
 	size_t input;
 	size_t output;
+	// Hooks allow customizability easily. deriv is for backward pass
+	HookFunc forwardHook;
+	HookFunc forwardHookDerivative;
 	float* quadratic() {return weightsBegin;};
 	float* inear() {return weightsBegin+(input*output);};
 	float* biases() {return weightsBegin+(input*output*2);};
@@ -17,6 +21,7 @@ struct Layer {
 	#ifdef TRAINING_ON
 	float* previous_inputs;
 	float* gradients;
+
 	void backward(float* upstream_grad) {
 	
 	}
